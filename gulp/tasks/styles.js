f655cb0c4a51a56b3ffe3ddcebfdde02d00cbad7
Weronika <poplawskaveronica@gmail.com>
2019-03-27 14:53:29 +0100

    added some changes to index.html

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,11 +3,12 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+cssImport = require('postcss-import'),
+cssMixins = require('postcss-mixins');
 
 gulp.task('styles', function() {
     return gulp.src('./app/assets/styles/styles.css')
-      .pipe(postcss([cssImport,cssvars, nested, autoprefixer]))
+      .pipe(postcss([cssImport, cssMixins, cssvars, nested, autoprefixer]))
       .on('error',function(err){
         console.log(err.toString());
         this.emit('end');
